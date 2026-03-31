26116025dab4611dbb64e3892c1e67585b199696
Yann Lebecque <ylebecque@gmail.com>
2026-03-31 16:12:39 +0200

    MAJ de main.py (changement de filtres)

--- main.py
+++ main.py
@@ -5,39 +5,28 @@ import pandas as pd
 
 @task
 def fetch_data():
-    url = "https://randomuser.me/api/?results=10"
+    url = "https://randomuser.me/api/?results=100"
     response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return f"Erreur lors de la récupération des données : {response.status_code}"
+    response.raise_for_status()
+    return response.json()
 
 
 @task
 def transform_data(data):
-    try:
-        df = pd.json_normalize(data["results"])
-        df = df.query("`registered.age`>18")
-        return df
-    except Exception as e:
-        return f"Erreur lors de la transformation des données : {e}"
+    df = pd.json_normalize(data["results"])
+    df = df.query("`registered.age`>21")
+    return df
 
 
 @task
 def save_data(df):
     df.to_csv("output.csv", index=False)
-    return "Data saved to output.csv"
 
 
 @flow
 def etl_pipeline():
-    try:
-        data = fetch_data()
-        df = transform_data(data)
-        result = save_data(df)
-        return result
-    except Exception as e:
-        return f"Erreur durant l'exécution du pipeline : {e}"
+    data = fetch_data()
+    df = transform_data(data)
 
 
 if __name__ == "__main__":
